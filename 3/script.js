a90bf36327b1e06ee8dbdd10a300612b93d78883
CassiopeiaBassis <cassiopeia.b@gmail.com>
2019-06-18 15:31:56 +0200

    ok4

--- 3/script.js
+++ 3/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 	alert("Bienvenue");
 	$( "p" ).on("click",function() {
-    	$("p").addClass('red');
-	});
+    	if($("p").hasClass('red')){
+    		$("p").removeClass('red');
+    	}else{
+    		$("p").addClass('red');
+    	}
+  	});
 });
